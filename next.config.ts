4bb100744ea80da23a0325b212d581faefeeb9a1
Fedor <feador@protonmail.com>
2026-02-18 19:57:25 +0100

    Tauri fix

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.TAURI_ENV ? "" : "/regrow",
+  basePath: process.env.TAURI_ENV_PLATFORM ? "" : "/regrow",
 };
 
 export default nextConfig;
